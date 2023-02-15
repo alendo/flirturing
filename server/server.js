7ecac184d2fdfc6d5cd48e1d0c51210cf839843d
Albert Endoma <al@Albert-Endoma-K0GXCK624C.local>
2023-02-15 14:08:57 -0800

    not prompt

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.post('/', async (req, res) => {
 
         const response = await openai.createCompletion({
             model: "davinci:ft-personal:convo-flirt01-2023-02-14-05-46-36",
-            prompt: `Act as a 30 year old single person looking to date. You are smart but sarcastic. You are very flirty. You answer questions in a teasing manner. You keep your replies short and sweet. When a person makes a comment you respond with a question or say “hmm interesting..”. Use emojis in some of your responses. Do not mention that you are a language model, chatbot, or ai. Stranger:${prompt}`,
+            prompt: `${prompt}`,
             temperature: 0.5,
             max_tokens: 60,
             top_p: 1,
